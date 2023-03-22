bb76f669dbf23391cfc2f7b6deae3cc272b22f3d
Gwenael Cotton <gcotton.oclock@gmail.com>
2023-03-22 01:01:13 +0100

    refactor: create input component with error state

---
+++ components/Login/ErrorInput.tsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+
+interface Props {
+  errorMessage: string;
+  typeInput: string;
+  placeholder: string;
+  label: string;
+}
+
+export default function InputError ({errorMessage, typeInput, placeholder, label}: Props): JSX.Element {
+    const [error, setError] = useState(false);
+    const handleBlur = (event: React.FocusEvent<HTMLInputElement>) => {
+        const {value, type} = event.target;
+        if (type === typeInput && value === "") {
+          setError(true);
+        } else {
+          setError(false);
+        }
+      };
+    return (
+    <div className="mb-2">
+        <label className="block text-gray-700 text-sm font-bold mb-2">
+            {label}
+        </label>
+        <input 
+            required
+            type={typeInput}
+            className="border-solid border-2 rounded-lg p-3 w-full"
+            placeholder={placeholder}
+            onBlur={handleBlur}
+        />
+        {error && (
+            <p className="text-red-500 text-xs italic">
+                {errorMessage}
+            </p>
+        )}
+    </div>
+    )
+  };
